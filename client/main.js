4151bb8f1e14174457a15cfda8cc7468d6cc19e5
Shuai Zhu <szhu12@ucsc.edu>
2016-10-01 09:46:59 -0700

    first page

--- client/main.js
+++ client/main.js
@@ -10,10 +10,21 @@ Meteor.subscribe('lines', function() {
 });
 
 Template.home.onCreated(function homeOnCreated() {
+  console.log('rendered')
+  //this is like rendered
+  $('#lineRegister').on("change paste keyup click input", function(){
+    console.log('submit')
+    $('#lineRegisterForm').submit();
+  });
 
+  $("#lineRegisterForm").submit(function (e) {
+    console.log(e);
+    e.preventDefault();
+  });
 });
 
 Template.home.helpers({
+  //No need for jquery if I have handlebars?
   line: function(){
     return Lines.find({}).fetch()[0];
   }
