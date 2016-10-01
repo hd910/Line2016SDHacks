816dc6e613e24f854e8a577bd8672d8b86f7b715
Shuai Zhu <szhu12@ucsc.edu>
2016-10-01 10:11:43 -0700

    routing + 2 pages avalaible

--- client/main.js
+++ client/main.js
@@ -4,24 +4,37 @@ import { Lines } from '../imports/server.js';
 
 import './main.html';
 
-
-Meteor.subscribe('lines', function() {
-  console.log(Lines.find({}).fetch()[0]);
+FlowRouter.route('/', {
+  name: 'home',
+  action(params, queryParams) {
+    console.log("Looking at a list?");
+    BlazeLayout.render('layout1', { top: "header", main: "home" });
+  }
 });
 
-Template.home.onCreated(function homeOnCreated() {
+FlowRouter.route('/:_id', {
+  name: 'line',
+  action(params, queryParams) {
+    console.log(params)
+    BlazeLayout.render('layout1', { top: "header", main: "dashboard" });
+  }
+});
+
+Meteor.subscribe('lines', function() {
+  //console.log(Lines.find({}).fetch()[0]);
+});
+
+Template.home.rendered = function() {
   console.log('rendered')
   //this is like rendered
-  $('#lineRegister').on("change paste keyup click input", function(){
-    console.log('submit')
-    $('#lineRegisterForm').submit();
-  });
+  //$('#lineRegisterForm').submit();
 
   $("#lineRegisterForm").submit(function (e) {
     console.log(e);
+    console.log($('#lineRegister').val)
     e.preventDefault();
   });
-});
+};
 
 Template.home.helpers({
   //No need for jquery if I have handlebars?
