aca53fb5d7ae1459b11ef051ee71a38f62d09617
Shuai Zhu <szhu12@ucsc.edu>
2016-10-01 09:01:22 -0700

    DB is functional

--- server/main.js
+++ server/main.js
@@ -6,13 +6,28 @@ import { Mongo } from 'meteor/mongo';
 export const Lines = new Mongo.Collection('lines');
 
 Meteor.methods({
-  newLine(){
+  newLine(cName, cAddress){
     //relevant parameters
-    Lines.insert({});
+    Lines.insert({
+      name: cName,
+      address: cAddress,
+      lineSize: 0,
+    });
+  },
+  //store local instance
+  lineMinus(cName){
+    //easier to just do seperate methods for different operations?
+    //switch chase?
+    Lines.update({"name": cName}, {$inc: {views: -1}});
+  },
+  lineAdd(cName){
+    Lines.update({"name": cName}, {$inc: {views: -1}});
   }
 });
 
+//Needs db to store info about files
 Meteor.startup(() => {
+  //Meteor.call('newLine', 'Panda Express', 'Panda Express, 453 Horton Plaza, San Diego, CA 92101')
   // code to run on server at startup
   //Restivus API?
 
